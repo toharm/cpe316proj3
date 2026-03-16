66d93d994302dec9c01c701e6e8ebd34832cd223
Tohar Markovich <80293744+toharm@users.noreply.github.com>
2026-03-15 17:46:13 -0700

    Update adc_helpers.h

--- Core/Inc/adc_helpers.h
+++ Core/Inc/adc_helpers.h
@@ -8,12 +8,16 @@
 #ifndef INC_ADC_HELPERS_H_
 #define INC_ADC_HELPERS_H_
 
-#define BUFFER_SIZE 512
+#define BUFFER_SIZE 4096
 
+#define ADC_SAMPLE_RATE  1000.0f                     /* 1 kHz ADC     */
+#define BIN_RES          (ADC_SAMPLE_RATE / (float)N)    /* ~0.98 Hz/bin  */
+#define PID_RATE         1.0f                          /* 50 Hz loop    */
+#define ADVANCE          ((int)(ADC_SAMPLE_RATE / PID_RATE))  /* 80 samples */
+#define PID_DT           (1.0f / PID_RATE)
 
 extern volatile uint16_t adc_buffer[BUFFER_SIZE];
 
 void ADC_Init(void);
-
 
 #endif /* INC_ADC_HELPERS_H_ */
